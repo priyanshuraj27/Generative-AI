fe8cc9972b4d3a3352028bdf83885649acec0a79
priyanshuraj27 <priyanshuraj1250@gmail.com>
2025-10-06 00:38:03 +0530

    Add Chain of Thought and Few Shot prompting implementations

---
+++ weather-agent/main.py
@@ -0,0 +1,37 @@
+# Few Shot prompting
+from google import genai
+from dotenv import load_dotenv
+import os
+import requests
+load_dotenv()
+# The client gets the API key from the environment variable `GEMINI_API_KEY`.
+api_key = os.getenv("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
+def get_weather(city : str) :
+    url = f"https://wttr.in/{city}?format=3"
+    response = requests.get(url)
+    if response.status_code==200 :
+        return f"weather in {city} is {response.text}"
+    return "something went wrong"
+print(get_weather("goa"))
+# Few shot prompting means giving instruction to the model along with examples
+SYSTEM_PROMPT = """
+You only have to answer questions related to coding. For any other topic say sorry.
+
+Examples:
+Q: How are you?
+A: Sorry, I can only answer coding related questions.
+
+Q: What is an array?
+A: Array is a list of elements that can be integers, characters, or even other arrays.
+
+Q: What is tuple?
+"""
+user_question = "What is an array"
+full_prompt = SYSTEM_PROMPT+ f"\n\nQ:{user_question}\nA:"
+response = client.models.generate_content(
+    model="gemini-2.0-flash-exp",
+    contents=full_prompt
+)
+print(response.text)
